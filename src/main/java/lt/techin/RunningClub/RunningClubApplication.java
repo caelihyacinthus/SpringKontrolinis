e64a0c670ab5d5b70bb229537df5bccb24ed1503
vejas valiukonis <vejasva@gmail.com>
2025-02-17 09:15:34 +0200

    init

---
+++ src/main/java/lt/techin/RunningClub/RunningClubApplication.java
@@ -0,0 +1,13 @@
+package lt.techin.RunningClub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningClubApplication.class, args);
+	}
+
+}
